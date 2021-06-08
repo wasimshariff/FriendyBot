9cf327a110687fe56f6ccf418ae16a4797bddde6
Wasim Shariff <wasim.shariff@physiciansmutual.com>
2021-06-08 09:43:35 -0500

    TA9999 - checking in base project

--- src/app/components/message-form/message-form.component.ts
+++ src/app/components/message-form/message-form.component.ts
@@ -24,11 +24,12 @@ export class MessageFormComponent implements OnInit {
     this.message.timestamp = new Date();
     this.messages.push(this.message);
 
-    /*this.dialogFlowService.getResponse(this.message.content).subscribe(res => {
+    this.dialogFlowService.getResponse(this.message.content).subscribe(res => {
+      console.log(JSON.stringify(res));
       this.messages.push(
-        new Message(res.result.fulfillment.speech, 'assets/images/bot.png', res.timestamp)
+        new Message(res.queryResult.fulfillmentText, 'assets/images/bot.png', new Date())
       );
-    });*/
+    });
 
     this.message = new Message('', 'assets/images/user.png');
   }
--- src/app/services/dialogflow.service.ts
+++ src/app/services/dialogflow.service.ts
@@ -1,30 +1,26 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable()
 export class DialogflowService {
 
-  private baseURL: string = "https://api.dialogflow.com/v1/query?v=20150910";
-  private token: string = 'token';
+  private baseURL: string = "https://dialogflow.clients6.google.com/v2/projects/testwasim-wdtt/agent/sessions/27e870ad-6d8d-1ebb-567a-acf888d5b2d3:detectIntent";
 
   constructor(private http: HttpClient){}
 
-  public getResponse(query: string){
-    let data = {
-      query : query,
-      lang: 'en',
-      sessionId: '12345'
-    }
-    /*return this.http
-      .post(`${this.baseURL}`, data, {headers: this.getHeaders()})
-      .map(res => {
-        return res.json()
-      })*/
+  public getResponse(query: string): Observable<any> {
+    let postRequest = {
+      "query_input": {
+        "text": {
+          "text": query,
+          "language_code": "en-US"
+        }
+      }
+    };
+    const headers: { [name: string]: string } = {};
+    headers.Authorization = 'Bearer ya29.a0AfH6SMDmtGW8EXEwq2A5uGDp1TVgfCGBldYDMXBo4mHBS-jpc52rFXEXKDufwuV_p8OhnSH0hKIRs319UvXmZEGPizSL57rYSh6uHDyIJmsp2ZsuVH8AZTjHbyt0w9k2I3KLXasT9DlBfHzktYx5yI61D3XPbbUbDwr_rng-TXeU3lIIYJQxFUjVbN--YG8akxcYqq5_0bxIR0U2gGLAO1zKcZR33FyyAQHMAlsteyvNLvI';
+    return this.http.post(this.baseURL, postRequest, {headers: headers});
   }
 
-  public getHeaders(){
-    let headers = new Headers();
-    headers.append('Authorization', `Bearer ${this.token}`);
-    return headers;
-  }
 }
